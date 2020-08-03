8a71e2ddb2e6a80d36edb02217cc95d24681d6b6
Jonathan Portillo <jonathanportillo@MacBook-Pro.home>
2020-08-02 22:44:12 -0400

    made the api FINALLY work

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character";
 
 const App = () => {
+  const [starWarsData, setStartWarsData] = useState([]);
+
+  const effectFn = () => {
+    axios
+      .get("https://swapi.py4e.com/api/people/1")
+      .then((response) => {
+        console.log("Success", response);
+        setStartWarsData(response.data);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(effectFn, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character character={starWarsData} />
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
-// Write your Character component here
+import React, { useState } from "react";
+
+const Character = (props) => {
+  const { character } = props;
+  console.log({ character });
+  return (
+    <div>
+      This is where character Data Goes
+      <p>Name: {character.name}</p>
+    </div>
+  );
+};
+
+export default Character;
